35255a96910dda94068e8bcf655470beb76559ae
callerc1 <chris.caller@gmail.com>
2015-09-12 17:37:21 +0100

    more oh-my-zsh plugins.

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -11,4 +11,4 @@ ZSH_THEME="agnoster"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git brew sublime osx node npm nvm colorize ruby composer rbenv)
+plugins=(git brew sublime osx node npm nvm colorize ruby composer rbenv bundler)
