a60d21d64dc031d776dcb8aca5e3b1677e8ec4e4
Chris Caller <chris.caller@googlemail.com>
2019-11-18 16:55:59 +0000

    nuke things im not using

--- RVM/env.zsh
+++
@@ -1,7 +0,0 @@
-# check for RVM
-if [[ -a ~/.rvm ]]
-then
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-  export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-  source ~/.profile
-fi
