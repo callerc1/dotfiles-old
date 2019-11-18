a81f07098956e2bc60e654b0f9a84bcf1580eb46
Chris Caller <chris.caller@googlemail.com>
2019-11-18 17:02:05 +0000

    Brewfile all the things

---
+++ homebrew/env.zsh
@@ -0,0 +1,6 @@
+export HOMEBREW_CASK_OPTS="--fontdir=~/Library/Fonts/Homebrew"
+
+# Source chtf
+if [[ -f /usr/local/opt/chtf/share/chtf/chtf.sh ]]; then
+    source "/usr/local/opt/chtf/share/chtf/chtf.sh"
+fi
--- homebrew/update.sh
+++ homebrew/update.sh
@@ -12,5 +12,10 @@ print_block "Updating Homebrew"
 
 brew update && brew upgrade
 
+print_block "Updating Brewfile"
+
+HOMEBREW_BUNDLE_CASK_SKIP=true brew bundle dump --file=$DOTFILES/homebrew/Brewfile --force --no-lock --no-restart
+brew bundle list --file=$DOTFILES/homebrew/Brewfile --all
+
 # And we're done
 print_block_end
